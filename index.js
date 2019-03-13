35f28df72b27f8834c795168e644cab97449c80b
Justin <justin@justin.mac>
2019-03-13 10:55:10 -0400

    overwrite `install` to only build on windows, all other platforms will noop
    move nan to dependencies
    add cross-spawn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-var NativeExtension = require('bindings')('NativeExtension');
-module.exports = NativeExtension;
+const { platform } = require('os');
+
+module.exports = {
+  getWindowText: () => {}
+};
+
+if (platform() === 'win32') {
+  module.exports = require('bindings')('NativeExtension');
+}
